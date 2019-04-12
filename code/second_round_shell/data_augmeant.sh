b3cc7df2bc721f488a1140b604c595709ac35a56
monkeyDemon <anshengmath@163.com>
2019-04-12 11:42:11 +0800

    retrain

--- code/second_round_shell/data_augmeant.sh
+++ code/second_round_shell/data_augmeant.sh
@@ -6,7 +6,7 @@ python ../second_round_pyfile/data_augmentation_position.py --restricted_dir ../
 --img_save_dir ../../data/ProcessData/train_val_jpg \
 --input_json_path ../../data/Second_round_data/jinnan2_round2_train_20190401/train_restriction_fix.json \
 --output_json_path ../../data/ProcessData/train_position.json \
---generate_num 20000
+--generate_num 40000
 
 cp ../../data/ProcessData/restricted_sigmoid/* ../../data/ProcessData/train_val_jpg
 

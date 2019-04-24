656393c70f2084cc43bf409538c33aa25ad86d06
monkeyDemon <anshengmath@163.com>
2019-04-24 21:32:28 +0800

    final!

--- code/second_round_shell/preprocess_sigmoid.sh
+++ code/second_round_shell/preprocess_sigmoid.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-python ../second_round_pyfile/preprocess_sigmoid.py --src_dir ../../data/Second_round_data/jinnan2_round2_test_a_20190401 \
---dst_dir ../../data/ProcessData/jinnan2_round2_test_a_20190401_sigmoid
+#python ../second_round_pyfile/preprocess_sigmoid.py --src_dir ../../data/Second_round_data/jinnan2_round2_test_a_20190401 \
+#--dst_dir ../../data/ProcessData/jinnan2_round2_test_a_20190401_sigmoid
 
 
-python ../second_round_pyfile/preprocess_sigmoid.py --src_dir ../../data/Second_round_data/jinnan2_round2_train_20190401/restricted \
---dst_dir ../../data/ProcessData/restricted_sigmoid
+#python ../second_round_pyfile/preprocess_sigmoid.py --src_dir ../../data/Second_round_data/jinnan2_round2_train_20190401/restricted \
+#--dst_dir ../../data/ProcessData/restricted_sigmoid
 
-python ../second_round_pyfile/preprocess_sigmoid.py --src_dir ../../data/Second_round_data/jinnan2_round2_train_20190401/normal \
---dst_dir ../../data/ProcessData/normal_sigmoid
+#python ../second_round_pyfile/preprocess_sigmoid.py --src_dir ../../data/Second_round_data/jinnan2_round2_train_20190401/normal \
+#--dst_dir ../../data/ProcessData/normal_sigmoid
+
+python ../second_round_pyfile/preprocess_sigmoid.py --src_dir ../../data/Second_round_data/jinnan2_round2_test_b_20190424 \
+--dst_dir ../../data/ProcessData/jinnan2_round2_test_b_20190424_sigmoid
